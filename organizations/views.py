61db1657cb334a1a6e223cc319f2439487829b89
BeshoiBotros <beshoibotros111@gmail.com>
2024-06-05 20:10:50 +0200

    .

--- organizations/views.py
+++ organizations/views.py
@@ -91,7 +91,7 @@ class ObjectOwnershipView(APIView):
     def get(self, request, pk=None, organization_pk=None):
         
         if organization_pk:
-            organization = ContentType(models.Organization, id=organization_pk)
+            organization = shortcuts.get_obj_by_kwargs(model=models.Organization, pk=organization_pk)
             queryset = models.ObjectOwnership.objects.filter(organization=organization)
             serializer = serializers.OwnershipSerializer(queryset, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
